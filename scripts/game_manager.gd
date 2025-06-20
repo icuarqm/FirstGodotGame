9ee9fa676897772d6404ce61c6d3c9e007a018e8
icuarqm <ali.oguz0119@gmail.com>
2025-06-20 05:18:00 +0300

    music and some minor updates

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -1,13 +1,21 @@
 extends Node
 
 var score = 0
+const TOTAL_COINS = 9
 
 @onready var score_label: Label = $ScoreLabel
 
 func add_point():
 	score += 1
-	if score == 1:
-		score_label.text = "You collected " + str(score) + " coin."
-	else:
-		score_label.text = "You collected " + str(score) + " coins."
+	update_score_text()
 	print(score)
+	
+func update_score_text():
+	var remaining = TOTAL_COINS - score
+	match score:
+		1:
+			score_label.text = "You collected 1 coin. 8 more to go!"
+		TOTAL_COINS:
+			score_label.text = "All coins collected!"
+		_:
+			score_label.text = "You collected %d coins. %d more to go!" % [score, remaining]
