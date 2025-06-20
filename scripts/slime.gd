74750d098c158a8290fe240802801cd0a5cc4bf6
icuarqm <ali.oguz0119@gmail.com>
2025-06-20 03:14:37 +0300

    slime updates

--- scripts/slime.gd
+++ scripts/slime.gd
@@ -1,11 +1,19 @@
 extends Node2D
 
+const SPEED = 45
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+var direction = 1
 
+@onready var ray_cast_right: RayCast2D = $RayCastRight
+@onready var ray_cast_left: RayCast2D = $RayCastLeft
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if ray_cast_right.is_colliding():
+		direction = -1
+		animated_sprite.flip_h = true
+	if ray_cast_left.is_colliding():
+		direction = 1
+		animated_sprite.flip_h = false
+	position.x += direction * SPEED * delta
